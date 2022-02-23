c00046beb29c0aa68afd11d6f7e8a08ab0d36232
Clementina Mendes <clementinadesmen@gmail.com>
2022-02-23 15:39:30 +0100

    Deployment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def index
+        @users = User.all
+    end
+    
+    def show 
+        @user = User.find(params[:id])
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "Bienvenue sur BookTonCreneau!"
+            redirect_to user_url(@user)
+        else
+            render 'new'
+            flash[:error] = "Le formulaire contient des erreurs!"
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "pages#home"
+  get  '/signup',  to: 'users#new'
+  post   '/signup',   to: 'sessions#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
