c1e850ebece7fdfeff504d42c735e08857cdc190
Clementina Mendes <clementinadesmen@gmail.com>
2022-02-25 13:22:03 +0100

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,18 @@ class User < ApplicationRecord
   end
 
   # Remember a user in the database for use in persistent sessions.
-  def Remember
+  def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end   
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forget a user 
+  def forget 
+    update_attribute(:remember_digest, nil)
+  end
 end
+
